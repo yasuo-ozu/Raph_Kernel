7e591b57c69d1ef7ca517c677b07cb00f7527012
yozu <yasuo@ozu.email>
2017-01-04 05:59:21 +0900

    Changed tab to soft indent in disk.sh

--- disk.sh
+++ disk.sh
@@ -8,8 +8,8 @@ sudo mkdir -p $TMPDIR
 umount() {
     sudo umount ${MOUNT_DIR}
     sudo kpartx -d ${IMGFILE}
-	[ -f $IMAGE ] && sudo rm $IMAGE
-	sudo cp $IMGFILE $IMAGE
+    [ -f $IMAGE ] && sudo rm $IMAGE
+    sudo cp $IMGFILE $IMAGE
     sudo losetup -d /dev/loop[0-9] > /dev/null 2>&1 || return 0
 }
 
@@ -20,8 +20,8 @@ loopsetup() {
 }
 
 mount() {
-	[ -f $IMGFILE ] && sudo rm $IMGFILE
-	sudo cp $IMAGE $IMGFILE
+    [ -f $IMGFILE ] && sudo rm $IMGFILE
+    sudo cp $IMAGE $IMGFILE
     loopsetup
     sudo mkdir ${MOUNT_DIR}
     sudo mount -t ext2 ${MAPPEDDEVICE} ${MOUNT_DIR}
@@ -36,8 +36,8 @@ if [ $1 = "umount" ]; then
 fi
 
 if [ $1 = "grub-install" ]; then
-	[ -f $IMGFILE ] || sudo rm $IMGFILE
-	sudo cp $IMAGE $IMGFILE
+    [ -f $IMGFILE ] || sudo rm $IMGFILE
+    sudo cp $IMAGE $IMGFILE
     loopsetup
     sudo mkfs -t ext2 ${MAPPEDDEVICE}
     sudo mkdir -p ${MOUNT_DIR}
@@ -47,11 +47,11 @@ if [ $1 = "grub-install" ]; then
 fi
 
 if [ $1 = "make-diskimage" ]; then
-	[ -f $IMGFILE ] && sudo rm $IMGFILE
-	sudo dd if=/dev/zero of=$IMGFILE bs=1M count=20
-	sudo parted -s $IMGFILE mklabel msdos -- mkpart primary 2048s -1
-	sudo rm $IMAGE
-	sudo cp $IMGFILE $IMAGE
+    [ -f $IMGFILE ] && sudo rm $IMGFILE
+    sudo dd if=/dev/zero of=$IMGFILE bs=1M count=20
+    sudo parted -s $IMGFILE mklabel msdos -- mkpart primary 2048s -1
+    sudo rm $IMAGE
+    sudo cp $IMGFILE $IMAGE
 fi
 
 
